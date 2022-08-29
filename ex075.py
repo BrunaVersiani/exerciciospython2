d547db8bc3b46edd5273313f67019d33c37a1061
RayaneVersiani <76855528+RayaneVersiani@users.noreply.github.com>
2022-08-28 21:48:49 -0300

    estudando

--- ex075.py
+++ ex075.py
@@ -2,3 +2,17 @@
 A) Quantas vezes apareceu o valor 9.
 B) Em que posição foi digitado o primeiro valor 3.
 C) Quais foram os números pares.'''
+núm = (int(input('Digite o 1° número: ')),
+        int(input('Digite o 2° número: ')),
+        int(input('Digite o 3° número: ')),
+        int(input('Digite o 4° número: ')))
+print(f'Você digitou os valores: {núm}')
+print(f'O número 9 apareceu {núm.count(9)} vezes.')
+if 3 in núm:
+    print(f'O número 3 apareceu na posição {núm.index(3)+1}')
+else:
+    print('O número 3 não apareceu em nenhuma posição. ')
+print('Os números pares digitados foram: ', end='')
+for n in núm:
+    if n % 2 == 0:
+        print(n, end=' ')
--- ex076.py
+++ ex076.py
@@ -1,1 +1,21 @@
-''''''
+'''Exercício Python 076: Crie um programa que tenha uma tupla única com nomes de produtos e seus respectivos preços, na
+sequência. No final, mostre uma listagem de preços, organizando os dados em forma tabular.'''
+print('\033[36m-' * 40)
+print(f'{"LISTAGEM DE PREÇO":^40}')
+print('-' * 40)
+listagem = ('borracha', 0.50,
+            'lapis', 1.00,
+            'caneta', 2.00,
+            'papel', 20.00,
+            'caderno', 15.25,
+            'estojo', 15.60,
+            'tesoura', 6.80,
+            'cola', 4.75,
+            'apontador', 0.90,
+            'clipes', 3.25)
+for pos in range(0, len(listagem)):
+    if pos % 2 == 0:
+        print(f'{listagem[pos]:.<30}', end='')
+    else:
+        print(f'R${listagem[pos]:>8.2f}')
+print('-' * 40)
