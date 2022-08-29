ad560e63c1024c2496e998a513ca6c76cbe36f71
RayaneVersiani <76855528+RayaneVersiani@users.noreply.github.com>
2022-08-28 22:27:37 -0300

    estudando

--- ex077.py
+++ ex077.py
@@ -1,2 +1,11 @@
 '''Exercício Python 077: Crie um programa que tenha uma tupla com várias palavras (não usar acentos). Depois disso,
 você deve mostrar, para cada palavra, quais são as suas vogais.'''
+print('*' * 30)
+palavras = ('este', 'texto', 'sobreviveu', 'salto', 'para', 'tipografia', 'electrónica',
+            'essencialmente', 'inalterada', 'popularizada', 'anos', 'disponibilização','folhas',
+            'continham', 'passagens', 'recentemente', 'programas', 'publicação', 'como')
+for p in palavras:
+    print(f'\nNa palavra\033[36m {p.upper()}\033[m temos: ', end=' ')
+    for letra in p:
+        if letra.lower() in 'aeiou':
+            print(letra, end=' ')
