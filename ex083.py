cfeb187d5ae8079a5a608fe56eef0effcb2bd8e7
RayaneVersiani <76855528+RayaneVersiani@users.noreply.github.com>
2022-09-02 22:57:06 -0300

    Atualizando

--- ex083.py
+++ ex083.py
@@ -1,3 +1,17 @@
 '''Exercício Python 083: Crie um programa onde o usuário digite uma expressão qualquer que use parênteses. Seu aplicativo
  deverá analisar se a expressão passada está com os parênteses abertos e fechados na ordem correta.'''
-int(input())
+expressão = str(input('Digite a sua expressão: '))
+pilha = []
+for simbolos in expressão:
+    if simbolos == '(':
+        pilha.append('(')
+    elif simbolos == ')':
+        if len(pilha) > 0:
+            pilha.pop()
+        else:
+            pilha.append(')')
+            break
+if len(pilha) == 0:
+    print('Ok! Sua expressão está valida!')
+else:
+    print('Sua expressão esta errada!')
