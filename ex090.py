77cdbb1f7cc6fe06cd1a8170c47fae16029f7b24
RayaneVersiani <76855528+RayaneVersiani@users.noreply.github.com>
2022-09-11 16:04:21 -0300

    Atualizando

--- ex090.py
+++ ex090.py
@@ -1,7 +1,15 @@
 '''Exercício Python 090: Faça um programa que leia nome e média de um aluno, guardando também a situação em um dicionário.
 No final, mostre o conteúdo da estrutura na tela.'''
 aluno = dict()
-aluno['nome'] = str(input('Digite o nome do aluno: '))
-aluno['Média'] = float(input(f'Digite a média do {aluno["nome"]}:'))
-
-
+aluno['Nome'] = str(input('Digite o nome do aluno: '))
+aluno['Media'] = float(input(f'Digite a média do {aluno["Nome"]}: '))
+if aluno['Media'] >= 7:
+    aluno['situação'] = 'Aprovado(a)'
+elif 5 <= aluno['Media'] < 7:
+    aluno['situação'] = 'Recuperação'
+else:
+    aluno['situação'] = 'Reprovado(a)'
+print('*-' * 20)
+for k, v in aluno.items():
+    print(f'{k} é igual a {v} ')
+print('*-' * 20)
