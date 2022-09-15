beed3ecb9cc671983ac428f4d1c640297efa81ab
RayaneVersiani <76855528+RayaneVersiani@users.noreply.github.com>
2022-09-14 21:47:16 -0300

    '''Exercício Python 092: Crie um programa que leia nome, ano de nascimento e carteira de trabalho e cadastre-o (com idade)  em um dicionário. Se por acaso a CTPS for diferente de ZERO, o dicionário receberá também o ano de contratação e o salário.  Calcule e acrescente, além da idade, com quantos anos a pessoa vai se aposentar.'''

--- ex092.py
+++ ex092.py
@@ -1,6 +1,16 @@
 '''Exercício Python 092: Crie um programa que leia nome, ano de nascimento e carteira de trabalho e cadastre-o (com idade)
  em um dicionário. Se por acaso a CTPS for diferente de ZERO, o dicionário receberá também o ano de contratação e o salário.
  Calcule e acrescente, além da idade, com quantos anos a pessoa vai se aposentar.'''
+dados = dict()
+dados['nome'] = str(input('Nome: '))
+dados['idade'] = int(input('Ano nascimento: '))
+dados['ctps'] = int(input('CTPS: '))
+
+
+
 print('\033[36m*-' * 20)
+
+
+
 
 print('*-' * 20)
