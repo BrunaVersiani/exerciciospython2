db7eeb14d0b0f5b8ad2a06e1e235d32b8c257f59
Ray Bruna <rayaneversiani@gmail.com>
2022-09-20 11:12:40 -0300

    Update ex090.py
    '''Exercício Python 090: Faça um programa que leia nome e média de um aluno,
    guardando também a situação em um dicionário.
    No final, mostre o conteúdo da estrutura na tela.'''

--- ex090.py
+++ ex090.py
@@ -1,4 +1,5 @@
-'''Exercício Python 090: Faça um programa que leia nome e média de um aluno, guardando também a situação em um dicionário.
+'''Exercício Python 090: Faça um programa que leia nome e média de um aluno,
+guardando também a situação em um dicionário.
 No final, mostre o conteúdo da estrutura na tela.'''
 aluno = dict()
 aluno['Nome'] = str(input('Digite o nome do aluno: '))
